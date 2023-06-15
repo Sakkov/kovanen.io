127b83f10059172587e500978f9c58446ab7c829
Saku <96244618+Sakkov@users.noreply.github.com>
2023-06-15 15:02:51 +0300

    added kartoitus

--- Scripts/nav.js
+++
@@ -1,9 +0,0 @@
-function navBar(y) {
-    y.classList.toggle("change");
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
